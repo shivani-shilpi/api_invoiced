cd39043d3fc7ea48175ef2ec94ef630d313216b8
Shivani Shilpi <shivanishilpi2120@gmail.com>
2023-11-19 22:38:06 +0530

    Added crud and added rack-cors gem into gemfile for api into.

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,16 @@ module Invoiced2
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.api_only = true
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource( 
+          '*',
+          headers: :any, 
+          methods: [:get, :post, :patch, :put, :delete, :options]
+        )
+      end
+    end
   end
 end
